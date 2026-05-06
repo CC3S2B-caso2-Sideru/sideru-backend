22e62c8c8cbe37e8bbc7b284298e42591f14a07e
fausto-e <fausto4412711@gmail.com>
2026-05-06 16:10:54 -0500

    Create security package

--- src/main/java/com/sideru/sideru_backend/usuario/UsuarioRepository.java
+++ src/main/java/com/sideru/sideru_backend/usuario/UsuarioRepository.java
@@ -4,6 +4,9 @@ import com.sideru.sideru_backend.usuario.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByUsername(String username);
 }

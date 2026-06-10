55dce31686f67c8c128b0a6d72e662b45509a01d
fausto-e <fausto4412711@gmail.com>
2026-06-10 00:35:03 -0500

    Crear tests para el manejo de productos

--- src/test/java/com/sideru/sideru_backend/SideruBackendApplicationTests.java
+++ src/test/java/com/sideru/sideru_backend/SideruBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.sideru.sideru_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SideruBackendApplicationTests {
 
 	@Test
